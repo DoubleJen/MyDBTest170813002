e161a507ac4adf7a26840cb474a87168d0ce8d03
Chang <qoo2003654@gmail.com>
2017-08-13 13:55:11 +0800

    170813 1355

---
+++ app/src/main/java/tw/adouble/app/helloworld/mydbtest170813002/MainActivity.java
@@ -0,0 +1,27 @@
+package tw.adouble.app.helloworld.mydbtest170813002;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void insert(View view){
+
+    }
+    public void delete(View view){
+
+    }
+    public void update(View view){
+
+    }
+    public void query(View view){
+
+    }
+}
